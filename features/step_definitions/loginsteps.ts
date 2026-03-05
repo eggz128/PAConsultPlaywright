018c1a848ae016dc3c6a82573952e3505a372a08
Steve Powell <steve.powell@edgewords.co.uk>
2026-03-05 10:46:39 +0000

    Initial steps boilerplate

---
+++ features/step_definitions/loginsteps.ts
@@ -0,0 +1,16 @@
+import {Given, When, Then} from '@cucumber/cucumber'
+
+Given('the user is on the login page', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('the user enters valid credentials', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('the user should be redirected to the Add A Record page', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
